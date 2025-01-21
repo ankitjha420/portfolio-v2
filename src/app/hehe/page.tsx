720c49913b0edeb1b213988e1041083a47f3ca6c
Ankit <ankit.jha@deptagency.com>
2025-01-21 12:49:21 +0530

    formatting

---
+++ src/app/hehe/page.tsx
@@ -0,0 +1,10 @@
+const HehePage = () => {
+	return (
+		<main className="relative h-[200vh]">
+			<section className="h-screen bg-red-600 sticky top-0">first</section>
+			<section className="h-screen bg-green-900 relative">second</section>
+		</main>
+	)
+}
+
+export default HehePage
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
-export default function Loading(){
+export default function Loading() {
 
 	return (
-		<div id='loading-screen'>
+		<div id='loading-screen' className='h-full w-full flex items-center justify-center'>
 			<h1>LOADING...</h1>
 		</div>
 	)
